672d452344d5ab75cc26fd1ebb7e4cd285b5cef7
apiliuk <lohika.artur@98point6.com>
2019-12-19 14:55:26 +0200

    code for GRID

--- conftest.py
+++ conftest.py
@@ -1,9 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    # browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    grid_url = "http://localhost:4444/wd/hub"
+    desired_caps = DesiredCapabilities.CHROME
+    browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
     yield browser
     browser.quit()
