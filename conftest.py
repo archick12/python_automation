de7f8ae219f6da89fd850ff8bc200f5beecb5397
apiliuk <lohika.artur@98point6.com>
2020-02-24 13:54:37 +0200

    added fixture

--- conftest.py
+++ conftest.py
@@ -6,9 +6,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def browser():
-    browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    # grid_url = "http://localhost:4444/wd/hub"
-    # desired_caps = DesiredCapabilities.CHROME
-    # browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
+    # browser = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    grid_url = "http://localhost:4444/wd/hub"
+    desired_caps = DesiredCapabilities.CHROME
+    browser = webdriver.Remote(desired_capabilities=desired_caps, command_executor=grid_url)
     yield browser
     browser.quit()
